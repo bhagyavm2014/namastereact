50cfe9fed5b6ed4529ed8beed598821efbba6e6c
bhagya vardhan <vardhan@ladsolutions.in>
2024-01-17 23:09:18 +0530

    add first code

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-
+import React from "react";
+import ReactDOM from "react-dom"; 
 
 const parent = React.createElement("div",
 {id:"parent"},[
 React.createElement("div",{id:"child"},[
-React.createElement("h1",{},"This is h1 tag"),
-React.createElement("h2",{},"This is h1 tag") ]
+React.createElement("h1",{},"I am learning react js"),
+React.createElement("h2",{},"I am learning react js new") ]
 ),
 React.createElement("div",{id:"child2"},[
     React.createElement("h1",{},"This is h1 tag"),
